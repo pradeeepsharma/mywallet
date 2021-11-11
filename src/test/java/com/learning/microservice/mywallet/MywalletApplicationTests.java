1806fdd51cbe865bb5e2669b83b727bb6e29148b
Pradeep <pradeeepsharma@gmail.com>
2021-11-11 14:23:29 +0530

    Initial commit

---
+++ src/test/java/com/learning/microservice/mywallet/MywalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.microservice.mywallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
